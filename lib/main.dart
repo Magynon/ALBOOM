3360f8c5e456c67dae3bf71b52eb7d23888d3839
Magynon <stefanmagirescu@yahoo.com>
2021-07-12 15:38:39 +0300

    Updated documentation and comma in main :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 // @dart=2.9
-import 'package:app/screens/cart_screen.dart';
 import 'package:app/screens/current_screen.dart';
-import 'package:app/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -10,13 +8,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Music player',
-        debugShowCheckedModeBanner: false,
-        home: CurrentScreen(),
-        theme: ThemeData(
-          primaryColor: Colors.grey[900],
-          accentColor: Colors.lightGreen[800],
-          scaffoldBackgroundColor: Colors.grey[900],
-        ));
+      title: 'Music player',
+      debugShowCheckedModeBanner: false,
+      home: CurrentScreen(),
+      theme: ThemeData(
+        primaryColor: Colors.grey[900],
+        accentColor: Colors.lightGreen[800],
+        scaffoldBackgroundColor: Colors.grey[900],
+      ),
+    );
   }
 }
